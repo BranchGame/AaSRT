d4b3b6657c79bec9db9261e15d42d00709576ba7
BranchGame <97164990+BranchGame@users.noreply.github.com>
2022-11-13 13:27:51 +0300

    Delete Task3.java

--- Task3.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.lab1;
-
-public class Task3 {
-    public static void main(String[] args) {
-        for(String arg : args) {
-            System.out.println(arg);
-        }
-    }
-}
